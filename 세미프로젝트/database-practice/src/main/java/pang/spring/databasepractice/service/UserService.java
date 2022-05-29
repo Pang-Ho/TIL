dfcd9580fca7aa7e43490d992f034b630ae7be09
pang <slsl01@naver.com>
2022-05-30 02:06:07 +0900

    add user create function

--- 세미프로젝트/database-practice/src/main/java/pang/spring/databasepractice/service/UserService.java
+++ 세미프로젝트/database-practice/src/main/java/pang/spring/databasepractice/service/UserService.java
@@ -2,12 +2,14 @@ package pang.spring.databasepractice.service;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import pang.spring.databasepractice.dto.CreateUser;
 import pang.spring.databasepractice.dto.UserDto;
 import pang.spring.databasepractice.entity.User;
 import pang.spring.databasepractice.repository.UserRepository;
+import pang.spring.databasepractice.statusCode.Status;
 
 import javax.sql.DataSource;
-import java.sql.SQLException;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -15,19 +17,33 @@ public class UserService {
     DataSource dataSource;
     UserRepository userRepository;
 
-    public List<UserDto> users() throws SQLException {
+    public List<UserDto> users() {
         return userRepository.findAllUsers();
     }
 
-    public void create(User user) {
-
+    public CreateUser.Response create(CreateUser.Request request) {
+        User user = new User();
+        user.setEmail(request.getEmail());
+        user.setPw(request.getPw());
+        user.setPoint(0);
+        user.setStatus(Status.JOIN);
+        user.setCreatedAt(LocalDateTime.now());
+        user.setUpdatedAt(LocalDateTime.now());
+        return CreateUser.Response.fromEntity(userRepository.save(user));
     }
 
     @Transactional
-    public void accountPoint(String from, String to, int point) throws SQLException {
-        User fromUser = userRepository.findByEmail(from);
-        User toUser = userRepository.findByEmail(to);
-        userRepository.updatePoint(fromUser, fromUser.getPoint() - point);
-        userRepository.updatePoint(toUser, toUser.getPoint() + point);
+    public void accountPoint(String from, String to, int point) {
+        UserDto fromUser = userRepository.findByEmail(from);
+        fromUser.setPoint(fromUser.getPoint() - point);
+        UserDto toUser = userRepository.findByEmail(to);
+        toUser.setPoint(toUser.getPoint() + point);
+
+        userRepository.save(fromUser);
+        userRepository.save(toUser);
+    }
+
+    public UserDto findByEmail(String email) {
+        return userRepository.findByEmail(email);
     }
 }
